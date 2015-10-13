3767cac62a95ddb837dd02d5035d90b56db6086f
Kevin Townsend <kt@krtownsend.com>
2015-10-13 16:53:21 -0500

    .

--- verilog/xst.mk
+++ verilog/xst.mk
@@ -2,7 +2,10 @@ xst : pattern_decoder.prj
 	echo "run -ifn pattern_decoder.prj -ifmt mixed -top pattern_decoder -ofn pattern_decoder.ngc -iobuf NO -bufg 0 -iob false -ofmt NGC -p xc5vlx330-2 -opt_mode Speed -opt_level 1 -vlgincdir common" | xst
 
 pattern_decoder.prj :
-	echo "verilog work pattern_decoder.v" > pattern_decoder.prj
+	echo -e "verilog work pattern_decoder.v\n\
+verilog work std_fifo/std_fifo.v\n\
+verilog work asymmetric_fifo/asymmetric_fifo.v\n\
+verilog work ram/asymmetric_distributed_ram.v" > pattern_decoder.prj
 
 cleanXst :
 	rm -rf xst *.prj
